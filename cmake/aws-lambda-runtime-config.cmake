b555b2bc8c7deac585818960a05f3595073a4a52
Marco Magdy <mmagdy@gmail.com>
2018-11-17 16:40:04 -0800

    silly mistake

--- cmake/aws-lambda-runtime-config.cmake
+++ cmake/aws-lambda-runtime-config.cmake
@@ -3,7 +3,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
 set(AWS_LAMBDA_PACKAGING_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/packager)
 function(aws_lambda_package_target target)
     add_custom_target(aws-lambda-package-${target}
-        COMMAND ${AWS_LAMBDA_PACKAGING_SCRIPT}} $<TARGET_FILE:${target}> ${target}
+        COMMAND ${AWS_LAMBDA_PACKAGING_SCRIPT} $<TARGET_FILE:${target}> ${target}
         DEPENDS ${target})
 endfunction()
 
