a49facedc073b774ebb2d6d96c8bb759d361e3f4
Marco Magdy <mmagdy@gmail.com>
2018-11-17 16:38:15 -0800

    cleaning up the packaging script

--- cmake/AwsLambdaPackaging.cmake
+++
@@ -1,5 +0,0 @@
-function(aws_lambda_package_target target)
-    add_custom_target(NAME aws-lambda-package
-        COMMAND
-        ${CMAKE_CURRENT_LIST_DIR}/packager $<TARGET_FILE:${target}> ${target})
-endfunction()
--- cmake/aws-lambda-runtime-config.cmake
+++ cmake/aws-lambda-runtime-config.cmake
@@ -1,2 +1,9 @@
 include(${CMAKE_CURRENT_LIST_DIR}/@CMAKE_PROJECT_NAME@-targets.cmake)
 
+set(AWS_LAMBDA_PACKAGING_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/packager)
+function(aws_lambda_package_target target)
+    add_custom_target(aws-lambda-package-${target}
+        COMMAND ${AWS_LAMBDA_PACKAGING_SCRIPT}} $<TARGET_FILE:${target}> ${target}
+        DEPENDS ${target})
+endfunction()
+
