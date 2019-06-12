e54fa563c3ee9577067183ff3eea8cd5a654c7b4
Marco Magdy <mmagdy@gmail.com>
2019-06-12 19:11:35 +0000

    fixup! Add format attribute on logging functions

--- include/aws/logging/logging.h
+++ include/aws/logging/logging.h
@@ -27,8 +27,7 @@ enum class verbosity {
 
 void log(verbosity v, char const* tag, char const* msg, va_list args);
 
-[[gnu::format(printf, 2, 3)]]
-static inline void log_error(char const* tag, char const* msg, ...)
+[[gnu::format(printf, 2, 3)]] static inline void log_error(char const* tag, char const* msg, ...)
 {
     va_list args;
     va_start(args, msg);
@@ -38,8 +37,7 @@ static inline void log_error(char const* tag, char const* msg, ...)
     (void)msg;
 }
 
-[[gnu::format(printf, 2, 3)]]
-static inline void log_info(char const* tag, char const* msg, ...)
+[[gnu::format(printf, 2, 3)]] static inline void log_info(char const* tag, char const* msg, ...)
 {
 #if AWS_LAMBDA_LOG >= 1
     va_list args;
@@ -52,8 +50,7 @@ static inline void log_info(char const* tag, char const* msg, ...)
 #endif
 }
 
-[[gnu::format(printf, 2, 3)]]
-static inline void log_debug(char const* tag, char const* msg, ...)
+[[gnu::format(printf, 2, 3)]] static inline void log_debug(char const* tag, char const* msg, ...)
 {
 #if AWS_LAMBDA_LOG >= 2
     va_list args;
