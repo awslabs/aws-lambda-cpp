b7ba3625e1f8f7b45e474511ed3733c229c6b0fc
Bryan Moffatt <moffattb@amazon.com>
2020-04-04 00:22:25 +0100

    use platform auto-detection in backward.h

--- src/backward.h
+++ src/backward.h
@@ -43,7 +43,7 @@
 
 // You can define one of the following (or leave it to the auto-detection):
 //
-#    define BACKWARD_SYSTEM_LINUX
+//  #define BACKWARD_SYSTEM_LINUX
 //	- specialization for linux
 //
 // #define BACKWARD_SYSTEM_DARWIN
