fbfdaec9777adac9f6b50d9acd23edc24ca2b3e2
Marco Magdy <marcomagdy@users.noreply.github.com>
2023-02-14 13:01:15 -0800

    Fix clang-tidy benign warnings (#185)

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -25,6 +25,7 @@
 #include <cassert>
 #include <chrono>
 #include <array>
+#include <iterator>
 #include <cstdlib> // for strtoul
 #include <cinttypes>
 
@@ -133,12 +134,16 @@ static size_t read_data(char* buffer, size_t size, size_t nitems, void* userdata
     }
 
     if (unread <= limit) {
-        std::copy_n(ctx->first.begin() + ctx->second, unread, buffer);
+        auto from = ctx->first.begin();
+        std::advance(from, ctx->second);
+        std::copy_n(from, unread, buffer);
         ctx->second += unread;
         return unread;
     }
 
-    std::copy_n(ctx->first.begin() + ctx->second, limit, buffer);
+    auto from = ctx->first.begin();
+    std::advance(from, ctx->second);
+    std::copy_n(from, limit, buffer);
     ctx->second += limit;
     return limit;
 }
