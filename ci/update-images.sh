9f19102eb131b3f9bc9b381ea4f04bc8f497a465
Bryan Moffatt <bryan@bryanmoffatt.com>
2024-01-22 00:03:09 -0800

    idk

--- ci/update-images.sh
+++ ci/update-images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euox pipefail
 
 PRJ_ROOT=$(git rev-parse --show-toplevel)
 ECR_NAME=${ECR_NAME:-aws-lambda-cpp-ci}
@@ -12,21 +12,43 @@ aws ecr get-login-password --region $REGION | docker login --username AWS --pass
 # on Linux, if buildx is giving trouble - run:
 # docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-build-and-push () {
-    TAG=$ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_NAME:$1-$(echo $2 | sed 's|/|-|g')
-    docker build --platform $2 -t $TAG -f "$PRJ_ROOT/ci/docker/$1" .
-    docker push $TAG
+tag () {
+    echo $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$ECR_NAME:$1-$(echo $2 | sed 's|/|-|g')
 }
 
-if [[ $(arch) == "aarch64" ]]; then
+build-and-push () {
+    docker build --platform $2 -t $(tag $1 $2) -f "$PRJ_ROOT/ci/docker/$1" .
+    docker push $(tag $1 $2)
+}
+
+build () {
+    docker build --platform $2 --target $1 -t $(tag $1 $2) -f "$PRJ_ROOT/ci/docker/uberbase.Dockerfile" .
+}
+
+push () {
+    docker push $(tag $1 $2)
+}
+
+if [[ $(uname -m) == "aarch64" ]]; then
   build-and-push amazon-linux-2       linux/arm64
 else
-  build-and-push ubuntu-linux-18.04   linux/amd64 || echo "known broken build"
-  build-and-push ubuntu-linux-22.04   linux/amd64
-  build-and-push alpine-linux-3.15    linux/amd64
-  build-and-push alpine-linux-3.19    linux/amd64
-  build-and-push amazon-linux-2018.03 linux/amd64
-  build-and-push amazon-linux-2       linux/amd64
-  build-and-push amazon-linux-2023    linux/amd64
-  build-and-push arch-linux           linux/amd64
+  build all linux/amd64
+
+  build alpine-linux-3.15    linux/amd64
+  build alpine-linux-3.19    linux/amd64
+  build amazon-linux-2       linux/amd64
+  build amazon-linux-2018.03 linux/amd64
+  build amazon-linux-2023    linux/amd64
+  build arch-linux           linux/amd64
+  build ubuntu-linux-18.04   linux/amd64 || echo "known to be broken"
+  build ubuntu-linux-22.04   linux/amd64
+
+  push  alpine-linux-3.15    linux/amd64
+  push  alpine-linux-3.19    linux/amd64
+  push  amazon-linux-2       linux/amd64
+  push  amazon-linux-2018.03 linux/amd64
+  push  amazon-linux-2023    linux/amd64
+  push  arch-linux           linux/amd64
+  push  ubuntu-linux-18.04   linux/amd64
+  push  ubuntu-linux-22.04   linux/amd64
 fi
