0dd021069bc85b087d5d744cdc63906e2b0b9f12
Maxime David <maxday@amazon.com>
2026-07-01 10:14:48 +0000

    fix: jq

--- ci/integ/install-deps.sh
+++ ci/integ/install-deps.sh
@@ -5,18 +5,18 @@ OS=$1
 
 case "$OS" in
   al2023|al2023-arm)
-    dnf install -y cmake ninja-build gcc-c++ openssl-devel libcurl-devel zip tar gzip git
+    dnf install -y cmake ninja-build gcc-c++ openssl-devel libcurl-devel zip tar gzip git jq
     ;;
   ubuntu)
-    apt-get update && apt-get install -y git clang zlib1g-dev libssl-dev libcurl4-openssl-dev cmake ninja-build zip unzip curl
+    apt-get update && apt-get install -y git clang zlib1g-dev libssl-dev libcurl4-openssl-dev cmake ninja-build zip unzip curl jq
     update-alternatives --set cc /usr/bin/clang
     update-alternatives --set c++ /usr/bin/clang++
     ;;
   alpine)
-    apk add --no-cache bash cmake curl-dev curl g++ git ninja zlib-dev zip unzip tar
+    apk add --no-cache bash cmake curl-dev curl g++ git ninja zlib-dev zip unzip tar jq
     ;;
   arch)
-    pacman -Sy --noconfirm cmake ninja clang curl zip unzip git
+    pacman -Sy --noconfirm cmake ninja clang curl zip unzip git jq
     ;;
   *)
     echo "Unknown OS: $OS" && exit 1
